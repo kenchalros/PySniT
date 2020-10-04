8438727b96edff91bf912b8e0af01e631afba0eb
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-10-04 23:05:00 +0900

    delete file and move method

--- src/pysnit/pysnitlib/errors.py
+++ src/pysnit/pysnitlib/errors.py
@@ -1,2 +1,18 @@
+from functools import wraps
+
+
 class NotFoundBodyKey(Exception):
     pass
+
+
+def errmsghandler(e):
+    def _decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print('{}: {}'.format(e.__class__.__name__, e))
+            print('---')
+            func()
+            print('---')
+            exit(1)
+        return wrapper
+    return _decorator
